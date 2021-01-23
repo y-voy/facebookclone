54eb1752a215abbe0ac01bbf9b184869ee0c98d3
yuji moriya <voyager.nasa0928@gmail.com>
2021-01-23 22:36:01 +0900

    Picture controllerにて、newのaction&viewを追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,13 +1,21 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = Picture.all
   end
 
   def new
+    @picture = Picture.new
   end
 
   def create
-    Picture.create(content: params[:picture][:content])
+    Picture.create(picture_params)
     redirect_to new_picture_path
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:content)
   end
 
 end
