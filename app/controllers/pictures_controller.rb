50bad6e7de41cf1eab2814d543e2d2998ceb98b4
yuji moriya <voyager.nasa0928@gmail.com>
2021-01-23 19:50:32 +0900

    Pictures controllerにてcreateのactionを追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,5 +4,10 @@ class PicturesController < ApplicationController
 
   def new
   end
-  
+
+  def create
+    Picture.create(content: params[:picture][:content])
+    redirect_to new_picture_path
+  end
+
 end
