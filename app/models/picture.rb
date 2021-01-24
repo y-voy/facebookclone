7a09633024d1b0abd87eee6ce47cc62fe2445602
yuji moriya <voyager.nasa0928@gmail.com>
2021-01-24 23:13:29 +0900

    お気に入り登録機能の追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,4 +2,6 @@ class Picture < ApplicationRecord
   validates :content, presence: true, length: { in: 1..60000}
   mount_uploader :image, ImageUploader
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
 end
