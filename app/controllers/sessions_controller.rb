738b202811e6d9ab4731a3da5f7268d99b351ed3
yuji moriya <voyager.nasa0928@gmail.com>
2021-01-24 20:25:22 +0900

    PictureとUserに一 対多のアソシエーションを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      sessions[:user_id] = user.id
+      session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
       flash.now[:danger] = 'ログインに失敗しました'
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,5 @@
 class Picture < ApplicationRecord
   validates :content, presence: true, length: { in: 1..60000}
   mount_uploader :image, ImageUploader
+  belongs_to :user
 end
