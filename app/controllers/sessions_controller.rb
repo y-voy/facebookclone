970313dd148b91c45bff4fba3b3a553ee3066a50
yuji moriya <voyager.nasa0928@gmail.com>
2021-01-25 20:10:52 +0900

    エラーページの実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete(:user_id)
-    flash[:notice] = 'ログアウトしました'
+    flash.now[:notice] = 'ログアウトしました'
     redirect_to new_session_path
   end
 end
