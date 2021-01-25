d658c0286e51839c5dbbc6f44c70b0aa74536d48
yuji moriya <voyager.nasa0928@gmail.com>
2021-01-25 20:59:10 +0900

    画像アップロード時のリサイズを設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,10 @@
 class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [300, 150]
   storage :file
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
 end
