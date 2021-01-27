7f53fba4a9543ac449b1bc1250d7ae3b78e86593
yuji moriya <voyager.nasa0928@gmail.com>
2021-01-27 22:03:21 +0900

    いいねボタンをindexに表示し、いいねの数を表示

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,10 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(picture_id: params[:picture_id])
-    redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんのブログをお気に入り登録しました"
+    redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんの投稿に「いいね」をしました"
   end
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんのブログをお気に入り解除しました"
+    redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんの投稿の「いいね」を解除しました"
   end
 end
